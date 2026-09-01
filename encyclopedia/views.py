c467cef90a82c258f238c9bc875886f75a16e9dd
Rahixcode <mohammadabdulmomenin@gmail.com>
2026-09-02 00:58:55 +0600

    update 2

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import util
+import re
 
 
 def index(request):
@@ -16,6 +17,15 @@ def entry(request, title):
     else:
         return render(request, "encyclopedia/entry.html", {
             "title": title,
-            "content": content
+            "content": converter(content)
         })
-    
+
+def converter(text):
+    text = re.sub(r"^# (.+?)$", r"<h1>\1</h1>", text, flags=re.MULTILINE)
+    text = re.sub(r"^## (.+?)$", r"<h2>\1</h2>", text, flags=re.MULTILINE)
+    text = re.sub(r"^### (.+?)$", r"<h3>\1</h3>", text, flags=re.MULTILINE)
+    text = re.sub(r"^\*\*(.+?)\*\*$", r"<strong>\1</strong>", text, flags=re.MULTILINE)
+    text = re.sub(r"\n", r"<br>\n", text)
+    text = re.sub(r"^\[([a-zA-Z]+)\]\(/wiki/([a-zA-Z]+)\)$", r'<a href="/wiki/\2">\1</a>', text, flags=re.MULTILINE)
+
+    return text
