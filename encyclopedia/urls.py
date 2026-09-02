9b734c1cf4234d8ff8857c30bab556969c07ac30
Rahixcode <mohammadabdulmomenin@gmail.com>
2026-09-02 12:44:56 +0600

    update 3

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
 from . import views
 
+
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("random/", views.random_page, name="random"),
+    path("add_new/", views.add_page, name="page"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import util
 import re
+import random
 
 
 def index(request):
@@ -24,8 +25,22 @@ def converter(text):
     text = re.sub(r"^# (.+?)$", r"<h1>\1</h1>", text, flags=re.MULTILINE)
     text = re.sub(r"^## (.+?)$", r"<h2>\1</h2>", text, flags=re.MULTILINE)
     text = re.sub(r"^### (.+?)$", r"<h3>\1</h3>", text, flags=re.MULTILINE)
-    text = re.sub(r"^\*\*(.+?)\*\*$", r"<strong>\1</strong>", text, flags=re.MULTILINE)
+    text = re.sub(r"\*\*(.+?)\*\*", r"<strong>\1</strong>", text)
     text = re.sub(r"\n", r"<br>\n", text)
-    text = re.sub(r"^\[([a-zA-Z]+)\]\(/wiki/([a-zA-Z]+)\)$", r'<a href="/wiki/\2">\1</a>', text, flags=re.MULTILINE)
+    text = re.sub(r"\[([^\]]+)\]\(\/wiki\/([^)]+)\)", r'<a href="/wiki/\2">\1</a>', text)
 
     return text
+
+
+def random_page(request):
+    entries = util.list_entries()
+    if not entries:
+        return render(request, "encyclopedia/error.html", {
+            "error": "No encyclopedia entries exist yet."
+        })
+
+    title = random.choice(entries)
+    return redirect("wiki:entry", title=title)
+
+def add_page(request):
+    return render(request, "encyclopedia/new_page.html")
